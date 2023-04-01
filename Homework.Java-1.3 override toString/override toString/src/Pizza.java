7ad2f17c939f0fdd6e6f585b64ca0af1b135cf40
Mihnea Badea <115891777+miihnea@users.noreply.github.com>
2023-04-01 14:43:08 +0300

    Add files via upload

---
+++ Homework.Java-1.3 override toString/override toString/src/Pizza.java
@@ -0,0 +1,11 @@
+public class Pizza {
+    String crust = "thin";
+    String sauce = "tomato";
+    String topping = "meat";
+    int time = 30;
+
+    public String toString()
+    {
+        return crust+"\n"+sauce+"\n"+topping+"\n"+time;
+    }
+}
